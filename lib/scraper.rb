bbe6c8b790087fed99220a7893d161de79ac4283
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-03 11:57:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,18 +20,6 @@ class Scraper
 
 Scraper.new.get_page
 
-  def get_courses
-    self.get_page.css(".post")
-    end
-
-    def make_courses
-  self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
-  end
-end
 
   def print_courses
     self.make_courses
